efec0e72613a8e781850b7ca4bf656dfa5377991
Taylor Stallcup <tstallcup321@gmail.com>
2024-04-16 14:46:01 -0500

    mounted routes and created destinationsSchema

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema ({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    },
+    arrival: {
+        type: Date,
+        required: true,
+    }
+});
+
 const flightSchema = new Schema ({
     airline: {
         type: String,
@@ -22,7 +33,8 @@ const flightSchema = new Schema ({
         type: Date,
         required: true,
         default: () => new Date(new Date().setFullYear(new Date().getFullYear() + 1))
-    }
+    },
+    destinations: [ destinationSchema ]
 });
 
 module.exports = mongoose.model('Flight', flightSchema);
